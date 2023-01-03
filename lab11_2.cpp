fc551d7686c23d403dd76e31d375fc6633629068
PunnawichMai <punnawich_dachin@cmu.ac.th>
2023-01-03 20:13:08 +0700

    labb11.2 completed

--- lab11_2.cpp
+++ lab11_2.cpp
@@ -6,8 +6,18 @@ using namespace std;
 int main (){
 	ifstream source;
 	ofstream dest;
+	string text;
+
 	source.open("cheerbook.txt") ;
 	dest.open("cheerbook_copy.txt");
+	dest << "\"-------------------- BOOM ---------------------\"" ;
+	
+	while(getline(source,text))
+	{
+		dest << text << "\n";
+	}
+
+	dest << "\"-------------------- HA!! ---------------------\"";
 	
 	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
 	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
