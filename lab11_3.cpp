8565fe6d3d26515c1b3563768e0f0c52dd018d21
PunnawichMai <punnawich_dachin@cmu.ac.th>
2023-01-03 20:13:30 +0700

    lab11.3 completed

--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,34 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int i = 0;
+    double sum = 0;
+    double sum2 = 0;
+    string data;
+    ifstream source("score.txt");
+    ofstream dest("check.txt");
+
+    while(getline(source,data))
+    {
+        sum += atof(data.c_str());
+        sum2 += pow(atof(data.c_str()),2);
+        i++;
+    }
+    double mean,sd;
+    mean = sum/i;
+    sd = sqrt((sum2/i) - pow(mean,2));
+
+    cout << "Number of data = " << i << endl;
+    cout << "Mean = " << mean << endl;
+    cout << "Standard deviation = " << sd << endl;
+    //cout << "sum pow 2 / N = " << sd;
+
+    source.close();
+    dest.close();
+}
